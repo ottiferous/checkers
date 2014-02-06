8e6b306d926f260b1824e0523cba0f7b7dd0b0ee
Andrew Marrone <github@andrewmarrone.com>
2014-02-06 10:41:05 -0800

    implement piece move logic for sliding

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,9 +1,46 @@
 
 class Piece
 
+  attr_reader :color, :board, :position
   def initialize(color, board, position)
     @color, @position = color, position
+    @board = board
+  end
 
-    board.app_piece(self, pos)
+  # Black moves "down" the array if drawn
+  def slide_pos
+    @color == :black ? [[-1,1], [1,1]] : [[-1,-1], [1,-1]]
+  end
+
+  def perform_slide(end_pos)
+    start_pos = self.position
+    if valid_slides(start_pos, end_pos).include? end_pos
+      move_piece!(end_pos)
+    end
+  end
+    
+  def valid_slides(start_pos)
+    vectors = slide_pos
+    moves = []
+    vectors.each do |vec|
+      test = smush(vec, start_pos)
+      moves << test if @board.empty?(test)
+    end
+    moves
+  end
+  
+  # Already checked to see if its a valid move - now we do it.
+  def move_piece!(end_pos)
+    @board[end_pos] = self
+    @board[self.position] = nil
+    self.position = end_pos
+  end
+  
+  private
+  
+  def smush(x, y)
+    new_x = x[0] + y[0]
+    new_y = x[1] + y[1]
+    [new_x, new_y]
   end
 end
