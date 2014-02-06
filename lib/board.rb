de8160b3882ca33bdb5bd4d55c5b6cb4b1bafa9e
Andrew Marrone <github@andrewmarrone.com>
2014-02-06 11:12:18 -0800

    implement occupied? and color_at methods
    changed the color symbols from :black and :white to :white and :red

--- lib/board.rb
+++ lib/board.rb
@@ -7,10 +7,10 @@ class CheckersBoard
   end
 
   # replace with a render method later ( suprresses board info )
-  # def inspect
-  # 
-  #   nil
-  # end
+  def inspect
+  
+    nil
+  end
   
   def add_piece(piece, position)
     @rows[position[0]][position[1]] = piece.class.new(piece.color, piece.board, position)
@@ -18,26 +18,26 @@ class CheckersBoard
   
   def fill_in
 
-    # place back and front for :black
+    # place back and front for :white
     [0, 1].each do |row|
       (0..7).each do |x|
-        @rows[row][x] = Piece.new(:black, self, [row, x]) if x.odd?
+        @rows[row][x] = Piece.new(:white, self, [row, x]) if x.odd?
       end
     end
-    # place middle row for :black
+    # place middle row for :white
     (0..6).each do |x|
-      @rows[2][x] = Piece.new(:black, self, [1, x]) if x.even?
+      @rows[2][x] = Piece.new(:white, self, [1, x]) if x.even?
     end
     
     # place back and front for :red
     [5, 7].each do |row|
       (0..7).each do |x|
-        @rows[row][x] = Piece.new(:black, self, [row, x]) if x.even?
+        @rows[row][x] = Piece.new(:red, self, [row, x]) if x.even?
       end
     end
     # place middle row for :red
     (0..6).each do |x|
-      @rows[6][x] = Piece.new(:black, self, [6, x]) if x.odd?
+      @rows[6][x] = Piece.new(:red, self, [6, x]) if x.odd?
     end
       
   end
@@ -51,5 +51,12 @@ class CheckersBoard
     self[position].nil? ? true : false
   end
   
+  def occupied?(position)
+    self[position].nil? ? false : true
+  end
+  
+  def color_at(position)
+    self[position].color
+  end
   
 end
