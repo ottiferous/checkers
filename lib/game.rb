9943555b726d0844cb6c6771b266c8023a24aea6
Andrew Marrone <github@andrewmarrone.com>
2014-02-06 17:23:12 -0800

    fleshed out Game::play and Player attr_accessors

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,6 @@
-require "./board.rb"
-require "./piece.rb"
+require './piece.rb'
+require './board.rb'
+
 class OpponentPiece < ArgumentError; end
 class WrongHumanInput < ArgumentError; end
 class TargetOccupied < ArgumentError; end
@@ -8,16 +9,51 @@ class Game
 
   def initialize
     @board = Board.new
-    @player = Player.new
+    @players = []
   end
-
+  
   def play
+    @players << Player.new(:white)
+    @players << Player.new(:red)
+    finished = false
+    @board.draw
+
+    until finished
+      @players.each do |current_player|
+        puts "#{current_player.name} (#{current_player.color})"
+        begin 
+          move = current_player.get_move
+          start_pos = move.shift
+          raise OpponentPiece if @board.color_at(start_pos) != current_player.color          
+          @board[start_pos].perform_moves(move)
+        rescue OpponentPiece
+          puts "That's not your piece."
+          retry
+        rescue TargetOccupied
+          puts "You can't move to there."
+          retry
+        end
+        @board.draw
+      end
+      finished = self.game_over?(current_player.color)
+    end
+    puts "Game Over!"
+    
+  end
+  
+  def game_over?(color)
+    @board.all_of(color).each do |piece|
+      break if piece.has_moves?
+    
+      true
+    end
+  
   end
 end
 
 class Player
 
-  def initialize
+  attr_reader :color
   attr_accessor :name
   def initialize(color, name = "Player")
     @color = color
@@ -41,4 +77,11 @@ class Player
     [[start_x, start_y], [end_x, end_y]]
   end
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+  
+  g = Game.new
+  g.play
+  
 end
