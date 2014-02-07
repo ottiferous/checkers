b774ddcf19ace92eb6f6a8be11723232a5584d39
Andrew Marrone <github@andrewmarrone.com>
2014-02-06 17:22:47 -0800

    implements 3 new exceptions to raise

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,8 @@
 require "./board.rb"
 require "./piece.rb"
+class OpponentPiece < ArgumentError; end
+class WrongHumanInput < ArgumentError; end
+class TargetOccupied < ArgumentError; end
 
 class Game
 
@@ -15,7 +18,27 @@ end
 class Player
 
   def initialize
+  attr_accessor :name
+  def initialize(color, name = "Player")
+    @color = color
+    @name = name + rand(100).to_s
+  end
+  
+  def get_move
+    print "Your move: "
+    regex = /^(\d),(\d).*(\d),(\d)\s*$/
+    
+    begin
+      coordinates = gets.chomp.match(regex).to_a
+      coordinates.shift
+      coordinates.map!(&:to_i)
+      raise WrongHumanInput if coordinates.any? { |num| num > 8 || num < 0 }
+    rescue WrongHumanInput
+      puts "Did you make a typo?\n> "
+      retry
+    end
+    start_x, start_y, end_x, end_y = coordinates
+    [[start_x, start_y], [end_x, end_y]]
   end
 
 end
-
